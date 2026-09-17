940964ea24c67484c2c2b929e26cf32232caca29
rebaz-tv-app <rebazahmad19992@gmail.com>
2026-09-17 15:15:18 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -53,7 +53,7 @@ class MainActivity : ComponentActivity() {
                     // ئەگەر چالاک نەکرابوو، شاشەی قوفڵەکە دەکرێتەوە
                     ActivationScreen(
                         onActivated = {
-                            isActivated = tru
+                            isActivated = true
                         }
                     )
                 }
