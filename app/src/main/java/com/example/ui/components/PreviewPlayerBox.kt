7af91fa19404f7195e234a5a8e746b8d204f89f7
rebaz-tv-app <rebazahmad19992@gmail.com>
2026-09-25 00:52:50 +0300

    Update PreviewPlayerBox.kt

--- app/src/main/java/com/example/ui/components/PreviewPlayerBox.kt
+++ app/src/main/java/com/example/ui/components/PreviewPlayerBox.kt
@@ -177,7 +177,7 @@ fun PreviewPlayerBox(
                     modifier = Modifier
                         .align(AbsoluteAlignment.TopRight)
                         .absolutePadding(top = 4.dp, right = 14.dp) // ژمارەکە کەمکرایەوە تا بچێتە ڕاست
-                        .width(55.dp) 
+                        .width(60.dp) 
                         .height(26.dp)
                 )
             }
