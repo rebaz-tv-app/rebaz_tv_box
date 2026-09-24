ed24144c0d8948b3408282a654bda9a496fc1950
rebaz-tv-app <rebazahmad19992@gmail.com>
2026-09-24 07:11:48 +0300

    Update PreviewPlayerBox.kt

--- app/src/main/java/com/example/ui/components/PreviewPlayerBox.kt
+++ app/src/main/java/com/example/ui/components/PreviewPlayerBox.kt
@@ -27,6 +27,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -167,13 +168,12 @@ fun PreviewPlayerBox(
                 }
             }
 
-            // ⭐ لۆگۆکە هەمیشە دەردەکەوێت و سەد لە سەد دەچێتە لای ڕاست
             if (logoResId != null) {
                 Image(
                     painter = painterResource(id = logoResId),
                     contentDescription = "Channel Logo",
                     modifier = Modifier
-                        .align(Alignment.TopRight)
+                        .align(AbsoluteAlignment.TopRight)
                         .absolutePadding(top = 10.dp, right = 25.dp)
                         .width(85.dp)
                         .height(35.dp)
