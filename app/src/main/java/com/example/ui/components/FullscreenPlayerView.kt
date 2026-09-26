b534235d3a5a37d2407842394387d52cc0889f1e
rebaz-tv-app <rebazahmad19992@gmail.com>
2026-09-26 14:12:09 +0300

    Update FullscreenPlayerView.kt

--- app/src/main/java/com/example/ui/components/FullscreenPlayerView.kt
+++ app/src/main/java/com/example/ui/components/FullscreenPlayerView.kt
@@ -92,42 +92,42 @@ fun FullscreenPlayerView(
             logoWidth = 105.dp
             logoHeight = 27.dp
             logoTop = 15.dp
-            logoRight = 115.dp
+            logoRight = 113.dp
         }
         "Rebaz Sport 2" -> {
             logoResId = R.drawable.rebaz_sport_2
             logoWidth = 105.dp
             logoHeight = 27.dp
             logoTop = 15.dp
-            logoRight = 115.dp
+            logoRight = 113.dp
         }
         "Rebaz Sport 3" -> {
             logoResId = R.drawable.rebaz_sport_3
             logoWidth = 105.dp
             logoHeight = 27.dp
             logoTop = 15.dp
-            logoRight = 115.dp
+            logoRight = 113.dp
         }
         "Rebaz Sport 4" -> {
             logoResId = R.drawable.rebaz_sport_4
             logoWidth = 105.dp
             logoHeight = 27.dp
             logoTop = 15.dp
-            logoRight = 115.dp
+            logoRight = 113.dp
         }
         "Rebaz Sport 5" -> {
             logoResId = R.drawable.rebaz_sport_5
             logoWidth = 105.dp
             logoHeight = 27.dp
-            logoTop = 9.dp
-            logoRight = 115.dp
+            logoTop = 8.dp
+            logoRight = 113.dp
         }
         "Rebaz WWE" -> {
             logoResId = R.drawable.rebaz_wwe
             logoWidth = 105.dp
             logoHeight = 27.dp
             logoTop = 12.dp
-            logoRight = 115.dp
+            logoRight = 113.dp
         }
     }
 
